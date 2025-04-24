282edbcaa6a1ab8a7af0b516d19ce727ab4743e8
Daniel Crocker <crockdc@gmail.com>
2025-04-24 12:43:47 -0400

    Create repeatString function which repeats a string a number of times.
    Check that the argument is greater than zero, if not, return ERROR. Create a For Loop which iterates however many times requested and appends the string to a string, then return that combined string.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+    var combinedWord = "";
+    if (num < 0)
+        return "ERROR"
+    for (i = 0; i < +num; i++)
+        combinedWord += word;
+    return combinedWord;
 };
 
 // Do not edit below this line
